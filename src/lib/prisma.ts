596fac8b9034d3ccf756b6b2431c4b4627d29a69
RanjanaRK <ranjanakumari.me@gmail.com>
2025-08-16 21:28:34 +0530

    update prisma file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const globalForPrisma = global as unknown as {
-//   prisma: PrismaClient;
-// };
-
-// const prisma = globalForPrisma.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// export default prisma;
-
-// import { PrismaClient } from "../app/generated/prisma";
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
