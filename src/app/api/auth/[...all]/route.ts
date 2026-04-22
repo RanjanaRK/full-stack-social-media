d24661fa091507f90c9b357716f71957486534f0
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-22 21:21:43 +0530

    Update route.ts

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -4,15 +4,13 @@ export const runtime = "nodejs";
 export async function GET(req: Request) {
   const { auth } = await import("@/lib/auth");
   const { toNextJsHandler } = await import("better-auth/next-js");
-
-  const handler = toNextJsHandler(auth); // 👈 create once inside function
-  return handler.GET(req); // 👈 call after
+  const { GET } = toNextJsHandler(auth);
+  return GET(req);
 }
 
 export async function POST(req: Request) {
   const { auth } = await import("@/lib/auth");
   const { toNextJsHandler } = await import("better-auth/next-js");
-
-  const handler = toNextJsHandler(auth);
-  return handler.POST(req);
+  const { POST } = toNextJsHandler(auth);
+  return POST(req);
 }
